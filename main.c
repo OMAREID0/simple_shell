4541e91a1b8117e3f51854e6d1802ded814b5e65
omar eid <omareid221b@gmail.com>
2023-10-20 14:42:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
 #include "main.h"
+
 /**
- * main - simple shell project
- * @argc: number of argument pass to program
- * @argv: array of arguments.
+ * main - Simple shell project
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments
  *
- * Return: return 0 when exit.
+ * Return: 0 when exiting
  */
 int main(int argc, char *argv[])
 {
-	char *command = NULL;
-	dir_c *head = NULL;
-	int num_op = 0;
+    char *command = NULL;
+    dir_c *head = NULL;
+    int num_op = 0;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			printf("$ ");
-			fflush(stdout);
-		}
-		command = _getline(STDIN_FILENO);
-		if (!command)
-		break;
-		num_op++;
-		tokenization(command, &head, argv, num_op);
-		if (argc > 1000)
-		{
-			printf("a7a bt3ml eh");
-		}
-	}
-	return (0);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+        {
+            printf("$ ");
+            fflush(stdout);
+        }
+        command = _getline(STDIN_FILENO);
+        if (!command)
+            break;
+        num_op++;
+        tokenization(command, &head, argv, num_op);
+        if (argc > 1000)
+        {
+            printf("a7a bt3ml eh");
+        }
+    }
+    return (0);
 }
-
