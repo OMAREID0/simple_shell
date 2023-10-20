d3b97e62e3b7de8ac8a4fc63db13ea20fe79e73c
omar eid <omareid221b@gmail.com>
2023-10-20 14:36:05 +0300

    Update ch_syntax.c

--- ch_syntax.c
+++ ch_syntax.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * chdirf - Change the current working directory based on user input.
- * @p: the destination directory between [].
+ * ch_syntax - Change the current working directory based on user input.
+ * @p: The destination directory in square brackets.
  * @head: A pointer to the head of a directory history linked list.
- * @current_dir: pointer to the directory to store.
+ * @current_dir: Pointer to the directory to store.
  *
- * Return: retutn always (0).
+ * Return: Always returns (0).
  */
 int ch_syntax(char *p, dir_c **head, char *current_dir)
 {
-		if (chdir(p) != 0)
+	if (chdir(p) != 0)
+	{
+		perror("cd: ");
+	}
+	else
+	{
+		if (getcwd(current_dir, 1024) != NULL)
 		{
-			perror("cd: ");
+			insertc_dir(head, current_dir);
 		}
-		else
-		{
-			if (getcwd(current_dir, 1024) != NULL)
-			{
-				insertc_dir(head, current_dir);
-			}
-			if (*head == NULL)
-				return (0);
-			printf("%s", (*head)->dir);
-		}
+		if (*head == NULL)
+			return (0);
+		printf("%s", (*head)->dir);
+	}
 	return (0);
 }
