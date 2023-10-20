66e10956fb4b2da1570c466f49c8cfac0f17f3cb
omar eid <omareid221b@gmail.com>
2023-10-20 14:43:11 +0300

    Update main.h

--- main.h
+++ main.h
@@ -10,17 +10,17 @@
 #include <limits.h>
 
 #define MAX_LINE_LENGTH 256
-
 #define MAX_ARGS 10
+
 extern char **environ;
 
 typedef struct directories
 {
-	char *dir;
-	struct directories *next;
+    char *dir;
+    struct directories *next;
 } dir_c;
 
-/** string function */
+/** String Functions **/
 char *_strchr(char *s, char c);
 int _strlen(const char *s);
 char *_strcat(char *dest, char *src);
@@ -28,14 +28,15 @@ int _strcmp(char *s1, char *s2);
 char *_strdup(char *origin);
 char *_strcpy(char *dest, char *src);
 char *_getline(int fd);
-/** Helper functions */
+
+/** Helper Functions **/
 int is_executable(char *full_path);
 int execute(char *full_path, char *argvec[]);
 int tokenization(char *command, dir_c **head, char **argv, int num_op);
 int ch_previous(dir_c **head, char *current_dir);
 int ch_syntax(char *p, dir_c **head, char *current_dir);
 void insertc_dir(dir_c **head, char *current);
-int ch_home(dir_c **head, char *curreant_dir);
+int ch_home(dir_c **head, char *current_dir);
 void free_dir(dir_c *head);
 int chdirf(char **directory, int n, dir_c **head);
 void exit_status(char **compare, dir_c *head);
